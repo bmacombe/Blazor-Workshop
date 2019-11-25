6ce0e7db512d17a4208f2bf6ce507fd39b86a72f
Brian Macomber <brian@agspectrum.com>
2019-11-25 11:42:30 -0600

    Start Part 2

---
+++ BandBookerDataTest/BandBookerDataTest/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using BandBookerData;
+using BandBookerData.Models;
+
+
+namespace BandBookerDataTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Un-comment to start over
+            //DataManager.DeleteAll();
+
+            // Add Guitar
+            var guitar = (from i in DataManager.Instruments
+                where i.Name == "Guitar"
+                select i).FirstOrDefault();
+            if (guitar == null)
+            {
+                guitar = new Instrument()
+                {
+                    Name = "Guitar"
+                };
+                DataManager.AddInstrument(guitar);
+            }
+            Console.WriteLine("Instrument: {0} with InstrumentId {1}",
+                guitar.Name, guitar.InstrumentId);
+
+            // Add Keyboards
+            var keyboards = (from i in DataManager.Instruments
+                          where i.Name == "Keyboards"
+                          select i).FirstOrDefault();
+            if (keyboards == null)
+            {
+                keyboards = new Instrument()
+                {
+                    Name = "Keyboards"
+                };
+                DataManager.AddInstrument(keyboards);
+            }
+            Console.WriteLine("Instrument: {0} with InstrumentId {1}",
+                keyboards.Name, keyboards.InstrumentId);
+        }
+    }
+}
