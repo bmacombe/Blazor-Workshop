dc16cc821e2e491d8753a3d16a9d7fa0b5450bb0
Brian Macomber <brian@agspectrum.com>
2019-11-25 10:18:56 -0600

    wip

--- BlazorWrokshop/BlazorWrokshop/Controllers/CustomerController.cs
+++ BlazorWrokshop/BlazorWrokshop/Controllers/CustomerController.cs
@@ -15,6 +15,39 @@ namespace BlazorWrokshop.Controllers
         [HttpGet]
         public IEnumerable<Customer> Get()
         {
+            return GetAllCustomers();
+        }
+
+        // GET: api/Customer/5
+        [HttpGet("{id}", Name = "Get")]
+        public Customer Get(int id)
+        {
+            var customers = GetAllCustomers();
+            return (from x in customers
+                where x.CustomerId == id
+                select x).FirstOrDefault();
+        }
+
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private IEnumerable<Customer> GetAllCustomers()
+        {
             var Customers = new List<Customer>();
             Customers.Add(new Customer
             {
@@ -37,32 +70,33 @@ namespace BlazorWrokshop.Controllers
                     CustomerId = 3,
                     Name = "Doo Fuss"
                 });
+
+
+            Customers.Add(
+                new Customer
+                {
+                    CustomerId = 4,
+                    Name = "Hugh Jass"
+                });
+
+
+            Customers.Add(
+                new Customer
+                {
+                    CustomerId = 5,
+                    Name = "Donatella Nawan"
+                });
+
+
+            Customers.Add(
+                new Customer
+                {
+                    CustomerId = 6,
+                    Name = "Pykop Andropov"
+                });
+
+
             return Customers;
-        }
-
-        // GET: api/Customers/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/Customers
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Customers/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
